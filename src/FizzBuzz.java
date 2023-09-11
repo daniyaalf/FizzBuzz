971e22b90195636864abd46d9a6579def4095ef9
daniyaalf14 <dquantum14@gmail.com>
2023-09-11 15:37:16 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -4,9 +4,12 @@
 class FizzBuzz {
 
     public static void main(String[] args) {
+        doFizzBuzz();
+    }
 
-        for (int i = 1; i < 100; i++) {
-
+    private static void doFizzBuzz() {
+        int i = 1;
+        while (i < 100) {
             // Find out which numbers divide i.
             boolean divisibleBy3 = i % 3 == 0;
             boolean divisibleBy5 = i % 5 == 0;
@@ -29,6 +32,7 @@ class FizzBuzz {
                 System.out.println(i);
 
             }
+            i++;
         }
     }
 }
